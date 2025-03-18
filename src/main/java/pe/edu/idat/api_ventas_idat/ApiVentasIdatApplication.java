016cacca3ca1fbb1110fb9db1e74a087b055e1ae
IdatCicloFinal <idat6to2025@outlook.com>
2025-03-17 21:08:14 -0500

    implementación de CORS de manera global para todo el proyecto

--- src/main/java/pe/edu/idat/api_ventas_idat/ApiVentasIdatApplication.java
+++ src/main/java/pe/edu/idat/api_ventas_idat/ApiVentasIdatApplication.java
@@ -2,12 +2,28 @@ package pe.edu.idat.api_ventas_idat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiVentasIdatApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiVentasIdatApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer(){
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/v1/**")
+						.allowedMethods("GET", "POST", "PUT", "PATCH")
+						//.allowedOrigins("http://front-admin.idat.pe");
+						.allowedOrigins("*");
+			}
+		};
 	}
 
 }
